74ced1ba20b31d4797872e7cedafe7345d312afa
ritik-morya <74968381+ritik-morya@users.noreply.github.com>
2022-02-18 15:20:46 +0530

    Add files via upload

---
+++ Day2/app.js
@@ -0,0 +1,18 @@
+
+
+
+document.querySelector(".b1").addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(".b1").style.color = 'red';
+});
+
+document.querySelector(".b1").addEventListener('mouseout', function handleMouseOut() {
+    document.querySelector(".b1").style.color = 'black';
+});
+
+document.querySelector(".b2").addEventListener('mouseover', function handleMouseOver() {
+    document.querySelector(".b2").style.color = 'red';
+});
+
+document.querySelector(".b2").addEventListener('mouseout', function handleMouseOut() {
+    document.querySelector(".b2").style.color = 'black';
+});
